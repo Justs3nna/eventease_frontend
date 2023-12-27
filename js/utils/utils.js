708efd258623e7684bec229ce378fe186e07032a
Justs3nna <laulita.andrew@yahoo.com>
2023-12-27 12:54:55 +0800

    S

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -4,7 +4,7 @@ import { setRouter } from "../router/router.js";
 setRouter();
 
 // Backend URL sa NGROK
-const backendURL = "https://7e2f-175-176-84-122.ngrok-free.app/eventease-backend/public";
+const backendURL = "https://f78d-175-176-84-122.ngrok-free.app/eventease-backend/public";
 
 // Notifications
 function successNotification(message, seconds = 0) {
